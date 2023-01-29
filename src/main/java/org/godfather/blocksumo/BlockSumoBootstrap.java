81f2582a6088015ad505be673ab8e1df2bf4e4fd
Giulio <87502898+godfatherguy@users.noreply.github.com>
2023-01-29 12:05:52 +0100

    Fixed: doDaylightCycle to false in loaded maps too.

--- src/main/java/org/godfather/blocksumo/BlockSumoBootstrap.java
+++ src/main/java/org/godfather/blocksumo/BlockSumoBootstrap.java
@@ -1,10 +1,6 @@
 package org.godfather.blocksumo;
 
-import org.bukkit.Difficulty;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.world.WorldLoadEvent;
 import org.godfather.blocksumo.api.Bootstrap;
-import org.godfather.blocksumo.api.server.events.ServerFastEvent;
 import org.godfather.blocksumo.bukkit.BlockSumoDescription;
 import org.godfather.blocksumo.bukkit.manager.BlockSumoConfigManager;
 import org.godfather.blocksumo.bukkit.manager.BlockSumoGameManager;
@@ -16,17 +12,6 @@ public class BlockSumoBootstrap extends Bootstrap {
         setConfigManager(new BlockSumoConfigManager(this));
         setGameManager(new BlockSumoGameManager(this));
         setDescription(new BlockSumoDescription());
-
-        registerVariable(ServerFastEvent.builder(WorldLoadEvent.class)
-                .consumer(event -> {
-                    if (event.getWorld() != null) {
-                        event.getWorld().setAutoSave(false);
-                        event.getWorld().setTime(1000);
-                        event.getWorld().setGameRuleValue("doDaylightCycle", "false");
-                        event.getWorld().setDifficulty(Difficulty.PEACEFUL);
-                        event.getWorld().setGameRuleValue("naturalRegeneration", "false");
-                    }
-                }).priority(EventPriority.MONITOR).build());
     }
 
     @Override
--- src/main/java/org/godfather/blocksumo/api/Bootstrap.java
+++ src/main/java/org/godfather/blocksumo/api/Bootstrap.java
@@ -1,10 +1,12 @@
 package org.godfather.blocksumo.api;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Difficulty;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.world.WorldLoadEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.godfather.blocksumo.api.config.ConfigManager;
 import org.godfather.blocksumo.api.game.GameDescription;
@@ -62,6 +64,17 @@ public abstract class Bootstrap {
         registerVariable(ServerFastEvent.builder(FoodLevelChangeEvent.class)
                 .consumer(event -> event.setCancelled(true))
                 .priority(EventPriority.HIGH).build());
+
+        registerVariable(ServerFastEvent.builder(WorldLoadEvent.class)
+                .consumer(event -> {
+                    if (event.getWorld() != null) {
+                        event.getWorld().setAutoSave(false);
+                        event.getWorld().setTime(1000);
+                        event.getWorld().setGameRuleValue("doDaylightCycle", "false");
+                        event.getWorld().setDifficulty(Difficulty.PEACEFUL);
+                        event.getWorld().setGameRuleValue("naturalRegeneration", "false");
+                    }
+                }).priority(EventPriority.MONITOR).build());
 
         loaded = true;
 
